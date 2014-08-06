b49b6b729fd50ecff8217cbf83a95cb6c808c36d
daniela <daniela@codeismo.com>
2014-08-06 17:20:09 -0500

    editor con busqueda para articulo

--- servidor.js
+++ servidor.js
@@ -46,16 +46,18 @@ app.get("/articulo/:articuloId([0-9]+)", function(req, res) {
 	//HACEMOS LA CONSULTA PARA BUSCAR EL PRIMER RENGLON
 	//buscamos el renglon cuyo id sea 1
 	modelos.Articulo.find({
-		where:{id:articuloId},
-		include:[{
-			model:modelos.Comentario,
-			as:"comentarios"
-		},{
-			model:modelos.Categoria,
-			as:"categorias"
-		},{
-			model:modelos.Usuario,
-			as:"usuario"
+		where : {
+			id : articuloId
+		},
+		include : [{
+			model : modelos.Comentario,
+			as : "comentarios"
+		}, {
+			model : modelos.Categoria,
+			as : "categorias"
+		}, {
+			model : modelos.Usuario,
+			as : "usuario"
 		}]
 	}).success(function(articulo) {
 		//SI NO ENCUENTRA NADA articulo == null
@@ -75,19 +77,21 @@ app.get("/articulo/:articuloId([0-9]+)", function(req, res) {
 app.get("/usuario", function(req, res) {
 
 	modelos.Usuario.find({
-		where:{id:2},
+		where : {
+			id : 2
+		},
 		//
-		include:[{
-			model:modelos.Articulo,
+		include : [{
+			model : modelos.Articulo,
 			//el parametro as debe de ser igual al del parametro
 			//as que usaron en principal.js
-			as:"articulos"
-		},{
-			model:modelos.DatosUsuario,
-			as:"datosUsuario"
+			as : "articulos"
+		}, {
+			model : modelos.DatosUsuario,
+			as : "datosUsuario"
 		}]
 	}).success(function(usuario) {
-		
+
 		//la variable usuario ya tiene los articulos de ese USUARIO
 		//por medio de usuario.articulos
 		res.render("usuario.html", {
@@ -105,18 +109,18 @@ app.get("/informes", function(req, res) {
 //--- SOLUCION EJERCCIIO FIND Y FINDALL ----
 // localhost:8080/blog?offset=3
 app.get("/blog", function(req, res) {
-	
+
 	//req.query == LES DA ACCESO A TODOS LOS PARAMETROS
 	//QUE VIENEN EN EL QUERY STRING
 	//si no se pone el query string de offset
 	//toma el valor de 0
-	
+
 	var offset = req.query.offset;
-	
-	//COMO IMPLEMENTAR UN PAGINADOR jquery 
+
+	//COMO IMPLEMENTAR UN PAGINADOR jquery
 	modelos.Articulo.findAll({
-		limit:3,
-		offset:offset
+		limit : 3,
+		offset : offset
 	}).success(function(articulos) {
 		//articulos == son los renglones que encontro el metodo
 		//findAll
@@ -140,18 +144,28 @@ app.get("/blog", function(req, res) {
 });
 
 //ARMANDO NUESTRO EDITOR DE ARTICULOS
-app.get("/articulo/1/editar",function(req,res){
-	
-	res.render("articulo_editar.html");
+app.get("/articulo/:articuloId([0-9]+)/editar", function(req, res) {
+
+	//recibimos el parametro para editar el articulo
+	var articuloId = req.params.articuloId;
+
+	modelos.Articulo.find(articuloId).success(function(articulo) {
+		res.render("articulo_editar.html",{
+			articulo:articulo
+		});
+	});
+
 });
 
 //en el formulario enviamos los datos como una peticion http-post
-app.post("/guardar-articulo",function(req, res){
-	
-	var titulo = req.body.titulo;	
+app.post("/guardar-articulo", function(req, res) {
+
+	//req.params = parametros en las rutas
+	//req.query = parametros en forma de query string
+	var titulo = req.body.titulo;
+	var contenido = req.body.contenido;
+
 	res.send("titulo:" + titulo);
-		
+
 });
-
-
 
